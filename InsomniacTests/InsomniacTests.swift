c0647fac949d6687431dbe8562e4f0d53ae78313
Michael Hulet <bestraysfan@me.com>
2016-04-29 20:00:00 -0500

    Initial Commit

---
+++ InsomniacTests/InsomniacTests.swift
@@ -0,0 +1,36 @@
+//
+//  InsomniacTests.swift
+//  InsomniacTests
+//
+//  Created by Michael Hulet on 4/29/16.
+//  Copyright © 2016 Michael Hulet. All rights reserved.
+//
+
+import XCTest
+@testable import Insomniac
+
+class InsomniacTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
