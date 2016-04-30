13d3c1e5ff521ad84f8bba2cc378cde31c5f1c01
Michael Hulet <bestraysfan@me.com>
2016-04-30 06:44:57 +0000

    Did fun stuff like colors

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Insomniac
 Going to sleep, but competitive
+https://preview.c9users.io/slydog123123/elisha_coad_website/An%208-bit%20Moon%20Rpg.html?_c9_id=livepreview11&_c9_host=https://ide.c9.io
--- Web/js/sleep.js
+++ Web/js/sleep.js
@@ -1,7 +1,7 @@
 var repeater = null;
 
 function updateScoreForSeconds(seconds){
-    document.getElementById("points").innerHTML = Math.floor(seconds / 60);
+    document.getElementById("points").innerHTML = Math.floor(seconds / 60).toLocaleString();
     var total = seconds;
     var hours = Math.floor(total / 1200);
     document.getElementById("hours").innerHTML = hours;
@@ -15,12 +15,30 @@ function updateScoreForSeconds(seconds){
 function dataSaved(){
     return typeof localStorage.secondsSlept !== undefined && !isNaN(localStorage.secondsSlept);
 }
+function updateStyleMode(style){
+    var toggleButton = document.getElementById("toggle-button");
+    var html = document.getElementsByTagName("html")[0];
+    if(style === "night"){
+        if(!toggleButton.classList.contains("night")){
+              toggleButton.classList.add("night");
+              toggleButton.classList.remove("day");
+              html.classList.add("night");
+              html.classList.remove("day");
+          }
+    }
+    else if(style === "day"){
+        toggleButton.classList.add("day");
+        toggleButton.classList.remove("night");
+        html.classList.add("day");
+        html.classList.remove("night");
+    }
+}
 window.onload = function(){
   if(dataSaved()){
       updateScoreForSeconds(parseInt(localStorage.secondsSlept));
   }
   document.getElementById("toggle-button").onclick = function(){
-      if(repeater == null){
+      if(repeater === null){
           repeater = setInterval(function(){
               if(dataSaved()){
                   localStorage.secondsSlept = parseInt(localStorage.secondsSlept) + 1;
@@ -31,11 +49,13 @@ window.onload = function(){
               updateScoreForSeconds(localStorage.secondsSlept);
           }, 1000);
           document.getElementById("toggle-button").innerHTML = "I just woke up";
+          updateStyleMode("night");
       }
       else{
           clearInterval(repeater);
           repeater = null
           document.getElementById("toggle-button").innerHTML = "I'm going to sleep";
+          updateStyleMode("day");
       }
   }
 };
